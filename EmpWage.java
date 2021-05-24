7beac515a76a7c7baa4eb6aedd7afb8fe0b39c59
Anand27git <anandkumar256k@gmail.com>
2021-05-24 17:48:52 +0530

    Emp Attendence

--- EmpWage.java
+++ EmpWage.java
@@ -4,5 +4,12 @@ public static void main (String args[]) {
 
 System.out.println("Welcome to Employee Wage calculation");
 
+int IS_FullTime = 1;
+double emplCheck = Math.floor(Math.random() *10) % 2;
+if(emplCheck == IS_FullTime)
+System.out.println("employee is present");
+else
+System.out.println("employee is absent");
+       }
 }
-}
+
